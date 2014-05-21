e071efb1e36a778650bbeab67b1a640ca58d9c01
Erik Ogenvik <erik.ogenvik@jayway.com>
2014-05-21 14:26:16 +0200

    Step 1.

--- .lab_post_check.sh
+++ .lab_post_check.sh
@@ -1,1 +1,4 @@
 #Allows you to perform extra checks after maven has built. Return anything other than 0 to fail the build.
+[ -f target/stackexchangeview-0.1.0-SNAPSHOT.jar ] && exit 0
+echo "Could not find required artifact target/stackexchangeview-0.1.0-SNAPSHOT.jar"
+exit 1
